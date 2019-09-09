f7e10019fa0555bc2876c8d710dd3840f73d6781
bdione <baba934@gmail.com>
2019-09-09 00:19:07 -0400

    did some math problems

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -5,12 +5,22 @@ package math.problems;
  */
 public class Factorial {
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         /*
          * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
-
+    public static void main(String args[]){
+        int b,facto=1;
+        int number=5;//It is the number to calculate factorial
+        for(b=1;b<=number;b++){
+            facto=facto*b;
+        }
+        System.out.println("Factorial of "+number+" is: "+facto);
     }
 }
+
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module CodingExamJuly2019 {
+    requires java.sql;
+    requires java.xml;
+    requires gson;
+    requires mongo.java.driver;
+    requires testng;
+}
