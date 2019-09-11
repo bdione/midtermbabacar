03fa889ce10964945101ce1dc3b6dc3c7c530e97
bdione <baba934@gmail.com>
2019-09-10 23:16:32 -0400

    Did more core java que

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,19 +1,37 @@
 package math.problems;
 
+import java.util.Scanner;
 public class MakePyramid {
     public static void main(String[] args) {
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
 
-        */
+         */
+
+
+        int i, space, rows, k = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number of Rows : ");
+        rows = scan.nextInt();
+        for (i = 1; i <= rows; i++) {
+            for (space = 1; space <= (rows - i); space++) {
+                System.out.print("  ");
+            }
+            while (k != (2 * i - 1)) {
+                System.out.print("* ");
+                k++;
+            }
+            k = 0;
+            System.out.println();
 
 
         }
-  }
+    }
+}
